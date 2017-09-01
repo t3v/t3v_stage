0bbec7a3af2afe8c79567599d3dbd32ff2af80a1
Maik Kempe <mkempe@bitaculous.com>
2017-09-01 14:47:09 +0200

    [➠] Implemented configurable image resize.

--- Classes/Controller/ContentElements/SliderContentElementController.php
+++ Classes/Controller/ContentElements/SliderContentElementController.php
@@ -25,11 +25,33 @@ class SliderContentElementController extends ContentElementController {
    * @return void
    */
   public function indexAction() {
-    $settings = $this->settings;
-    $uids     = $settings['slides'];
-    $slides   = $this->slideRepository->findByUids($uids);
+    $settings   = $this->settings;
+    $uids       = $settings['slides'];
+    $slides     = $this->slideRepository->findByUids($uids);
+    $responsive = $settings['responsive'];
+
+    $settings['responsive'] = $this->sortResponsiveSettingsByMinWidth($responsive, 'minWidth');
 
     $this->view->assign('settings', $settings);
     $this->view->assign('slides', $slides);
+  }
+
+  /**
+   * Helper function to sort the responsive settings by minimum width.
+   *
+   * @param array $settings The responsive settings
+   * @return array The sorted responsive settings by minimum width
+   */
+  protected function sortResponsiveSettingsByMinWidth($settings) {
+    if (!empty($settings)) {
+      usort($settings, function ($setting1, $setting2) {
+        $minWidth1 = $setting1['breakpoint']['minWidth'];
+        $minWidth2 = $setting2['breakpoint']['minWidth'];
+
+        return $minWidth1 <=> $minWidth2;
+      });
+    }
+
+    return $settings;
   }
 }
--- TODOS.md
+++ TODOS.md
@@ -1,5 +1,4 @@
 TODOS
 =====
 
-- [ ] Configurable image resize via TypoScript
 - [ ] Drop support for PHP 5.x
