95f8d9f521968dd5a426685da9e7e2c2bca7cd35
Maik Kempe <mkempe@bitaculous.com>
2017-11-14 18:04:48 +0100

    [➠] Refactored TypoScript.

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -1,3 +1,48 @@
-# === Includes ===
+# ---------------------------
+# | T3v Stage Configuration |
+# ---------------------------
 
-<INCLUDE_TYPOSCRIPT: source="FILE: ./Vendor/setup.ts">
+# === Plugin Configuration ===
+
+plugin {
+  tx_t3vstage {
+    persistence {
+      classes {
+        T3v\T3vStage\Domain\Model\Slide {
+          mapping {
+            tableName = tx_t3vstage_domain_model_slide
+
+            # columns {
+            #   foo.mapOnProperty = foo
+            #   bar.mapOnProperty = bar
+            # }
+          }
+        }
+      }
+
+      enableAutomaticCacheClearing = {$plugin.tx_t3vstage.persistence.enableAutomaticCacheClearing}
+
+      storagePid = {$plugin.tx_t3vstage.persistence.storagePid}
+
+      updateReferenceIndex = {$plugin.tx_t3vstage.persistence.updateReferenceIndex}
+    }
+
+    settings {
+      # ...
+    }
+
+    view {
+      layoutRootPaths {
+        0 = {$plugin.tx_t3vstage.view.layoutRootPath}
+      }
+
+      templateRootPaths {
+        0 = {$plugin.tx_t3vstage.view.templateRootPath}
+      }
+
+      partialRootPaths {
+        0 = {$plugin.tx_t3vstage.view.partialRootPath}
+      }
+    }
+  }
+}
